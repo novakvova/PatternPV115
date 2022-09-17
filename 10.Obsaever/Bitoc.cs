741d365aaf7fd17255fd966200aa4f988bfa4add
novakvova <novakvova@gmail.com>
2022-09-17 18:31:12 +0300

    Working text file

---
+++ 10.Obsaever/Bitoc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Obsaever
+{
+    public class Bitoc
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; } //double
+        public override string ToString()
+        {
+            return $"{Id} '{Name}' {Price}";
+        }
+    }
+}
---
+++ 10.Obsaever/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace _10.Obsaever
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //WriteToFile();
+            //ReadFile();
+        }
+
+        private static void ReadFile()
+        {
+            using (var fs = new FileStream("dima.txt", FileMode.Open))
+            {
+                StreamReader sr = new StreamReader(fs, Encoding.UTF8);
+                Console.WriteLine(sr.ReadLine());
+                sr.Close();
+                fs.Close();
+            }
+        }
+        //Запис даних у файл
+        private static void WriteToFile()
+        {
+            Bitoc bitoc = new Bitoc
+            {
+                Id = 2321,
+                Name = "Біток",
+                Price = 19876.70m
+            };
+            using (var fs = new FileStream("dima.txt", FileMode.Create))
+            {
+                StreamWriter sw = new StreamWriter(fs,Encoding.UTF8);
+                sw.WriteLine(bitoc);
+                sw.Close();
+                fs.Close();
+            }
+            //
+        }
+    }
+}
